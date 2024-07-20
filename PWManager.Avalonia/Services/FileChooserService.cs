3305e48d732623c145986de5d1e109cba58e76bb
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-07-20 22:53:44 +0200

    finishes FileChooser implementation

--- PWManager.Avalonia/Services/FileChooserService.cs
+++ PWManager.Avalonia/Services/FileChooserService.cs
@@ -10,6 +10,12 @@ using System.Threading.Tasks;
 namespace PWManager.Avalonia.Services {
     internal class FileChooserService : IChooseFile {
         private readonly IVisualEnvironment _visEnv;
+        private FilePickerFileType Scuml { get; } = new("Scuml") {
+            Patterns = new[] { "*.scuml" },
+            AppleUniformTypeIdentifiers = new[] { "public.scuml" },
+            MimeTypes = new[] { "scuml/*" }
+        };
+
         public FileChooserService()
         {
             _visEnv = IoC.Resolve<IVisualEnvironment>();
@@ -17,17 +23,27 @@ namespace PWManager.Avalonia.Services {
         public async Task<string> OpenFileChooser() {
             var file = await _visEnv.CurrentTopLevel!.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions {
                 Title = "Open your scuml database",
-                AllowMultiple = false
+                AllowMultiple = false,
+                FileTypeFilter = new[] { Scuml }
             });
 
             if (file != null) {
-                return file.First().Path.ToString(); // Kommt in fehler wenn nichts ausgewählt...
+                var folder = await file.First().GetParentAsync();
+                return folder!.Path.LocalPath; // Kommt in fehler wenn nichts ausgewählt...
             }
             return "";
         }
 
-        public Task<string> SaveFileChooser() {
-            throw new System.NotImplementedException();
+        public async Task<string> SaveFileChooser() {
+            var file = await _visEnv.CurrentTopLevel!.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions {
+                Title = "Choose where your database will be stored",
+                AllowMultiple = false
+            });
+
+            if (file != null) {
+                return file.First().Path.LocalPath; // Kommt in fehler wenn nichts ausgewählt...
+            }
+            return "";
         }
 
     }
