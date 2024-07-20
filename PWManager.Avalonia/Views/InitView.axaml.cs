0ee176b278c7b43cf195de44072cb4b8285e2c53
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-07-20 22:52:31 +0200

    adds init view

---
+++ PWManager.Avalonia/Views/InitView.axaml.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+
+namespace PWManager.Avalonia.Views;
+
+public partial class InitView : UserControl {
+
+    public InitView() {
+        InitializeComponent();
+        this.SizeChanged += QuerySize;
+        loginBtn.Click += Clicked;
+    }
+
+    private void HighlightIfEmpty(object? sender, TextChangedEventArgs e) {
+        HighlightIfEmpty();
+    }
+
+    private void Clicked(object? sender, RoutedEventArgs e) {
+        HighlightIfEmpty();
+
+        nameBox.TextChanged += HighlightIfEmpty;
+        pwBox.TextChanged += HighlightIfEmpty;
+        pathBox.TextChanged += HighlightIfEmpty;
+        pwBoxRepeat.TextChanged += HighlightIfEmpty;
+    }
+
+    private void HighlightIfEmpty() {
+        if (String.IsNullOrWhiteSpace(pathBox.Text)) {
+            pathBox.Classes.Add("empty");
+        } else {
+            pathBox.Classes.Remove("empty");
+        }
+        if (String.IsNullOrWhiteSpace(nameBox.Text)) {
+            nameBox.Classes.Add("empty");
+        } else {
+            nameBox.Classes.Remove("empty");
+        }
+        if (String.IsNullOrWhiteSpace(pwBox.Text)) {
+            pwBox.Classes.Add("empty");
+        } else {
+            pwBox.Classes.Remove("empty");
+        }
+        if (String.IsNullOrWhiteSpace(pwBoxRepeat.Text)) {
+            pwBoxRepeat.Classes.Add("empty");
+        } else {
+            pwBoxRepeat.Classes.Remove("empty");
+        }
+    }
+
+    private void QuerySize(object? sender, SizeChangedEventArgs e) {
+        var size = e.NewSize;
+
+        if (size.Width > 500) {
+            container.Classes.Remove("small-layout");
+        } else {
+            container.Classes.Add("small-layout");
+        }
+    }
+}
---
+++ PWManager.UI/ViewModels/InitViewModel.cs
@@ -0,0 +1,70 @@
+﻿using PWManager.Application.Exceptions;
+using PWManager.Application.Services.Interfaces;
+using PWManager.Data.Abstraction;
+using PWManager.UI.Interfaces;
+
+namespace PWManager.UI.ViewModels {
+    public class InitViewModel : ViewModelBase {
+
+        private readonly IViewNavigator _viewNavigator;
+        private readonly ILoginService _loginService;
+        private readonly IChooseFile _fileChooserService;
+        private readonly IDatabaseInitializerService _dbInit;
+
+        public string DBPath { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string PasswordRepeat { get; set; } = "";
+
+        public InitViewModel(IViewNavigator viewNavigator) {
+            _loginService = IoC.Resolve<ILoginService>();
+            _fileChooserService = IoC.Resolve<IChooseFile>();
+            _dbInit = IoC.Resolve<IDatabaseInitializerService>();
+            _viewNavigator = viewNavigator;
+        }
+
+        public async void GetPath() {
+            var path = await _fileChooserService.SaveFileChooser();
+            if (!Path.Exists(path)) {
+                return;
+            }
+            DBPath = Path.GetFullPath(path);
+        }
+
+        public void Init() {
+            var path = DBPath;
+            if (!Path.Exists(path)) {
+                return;
+            }
+
+            try {
+                _dbInit.CheckIfDataBaseExistsOnPath(path);
+            } catch (UserFeedbackException ex) {
+                return;
+            }
+
+            if (!Password.Equals(PasswordRepeat)) {
+                return;
+            }
+
+            try {
+                _dbInit.InitDatabase(path, UserName!, Password!);
+            } catch (UserFeedbackException ex) {
+                return;
+            }
+
+            try {
+                var succ = _loginService.Login(UserName, Password, path);
+                if (!succ) {
+                    // show error message -> Username or password incorrect
+                    return;
+                }
+                ConfigFileHandler.WriteDefaultFile(UserName, path);
+                _viewNavigator.GoToPage<HomeViewModel>();
+            } 
+            catch (UserFeedbackException ex) {
+                // catch user feedback exception and show message
+            }
+        }
+    }
+}
