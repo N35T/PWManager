70bfb890c4d70792f13bf3e4542fa0e367b5a28b
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-07-20 22:52:43 +0200

    adds welcome view

---
+++ PWManager.Avalonia/Views/WelcomeView.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+
+namespace PWManager.Avalonia.Views;
+
+public partial class WelcomeView : UserControl {
+
+    public WelcomeView() {
+        InitializeComponent();
+        this.SizeChanged += QuerySize;
+    }
+
+    private void QuerySize(object? sender, SizeChangedEventArgs e) {
+        var size = e.NewSize;
+
+        if (size.Width > 500) {
+            container.Classes.Remove("small-layout");
+        } else {
+            container.Classes.Add("small-layout");
+        }
+    }
+}
---
+++ PWManager.UI/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PWManager.Application.Exceptions;
+using PWManager.Application.Services.Interfaces;
+using PWManager.Data.Abstraction;
+using PWManager.UI.Interfaces;
+
+namespace PWManager.UI.ViewModels {
+    public class WelcomeViewModel : ViewModelBase {
+
+        private readonly IViewNavigator _viewNavigator;
+
+        public WelcomeViewModel(IViewNavigator viewNavigator) {
+            _viewNavigator = viewNavigator;
+        }
+
+        public void Login() {
+            _viewNavigator.GoToPage<LoginViewModel>();
+        }
+
+        public void Init() {
+            _viewNavigator.GoToPage<InitViewModel>();
+        }
+    }
+}
